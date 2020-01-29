6b5568d92fb6f7afd68289177ddd49251f1a8703
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-01-29 23:15:31 +0000

    add photos and handle loading error

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function introProgressBar(tot, curr) {
     document.getElementById("intro__progress").style.width = percent + "%";
     document.getElementById("intro__progress__txt").innerHTML = addZeros(percent) + "%";
 
-    if (percent <= 100) {
+    if (percent >= 100) {
         document.querySelector(".watercolor_bg").style.display = "block";
         document.querySelector(".intro__progressbar").style.display = "none";
     }
@@ -31,37 +31,71 @@ function introProgressBar(tot, curr) {
 
 
 function loadImage(selector, url, callback) {
+    console.log(selector);
     try {
         const img = new Image();
         img.src = url;
+
         img.onload = function () {
             const elem = document.querySelector(selector);
             elem.style.backgroundImage = `url(${this.src})`;
             typeof callback === 'function' && callback();
         };
+
+        img.onerror = function () {
+            console.log("FASZ");
+            document.querySelector(".intro__progressbar h1").innerHTML = `ERROR: ${url} NOT FOUND`;
+        }
+
         return img;
-    } catch (error) { console.log(error) }
+    } catch (error) { console.log(error); }
 }
 
 function preLoadImages() {
     const imageObjs = [
-        { selector: ".watercolor_bg",/*    */ url: "/images/watercolor_bg.png" },
-        { selector: ".clockface", /*       */ url: "/images/analog-clockface.png" },
-        { selector: ".clock-helperline",/* */ url: "/images/clock_helperline.png" },
-        { selector: ".arc1",/*             */ url: "/images/arc1.png" },
-        { selector: ".arc2",/*             */ url: "/images/arc2.png" },
-        { selector: ".lion",/*             */ url: "/images/line_lion.png" },
-        { selector: ".palmtree",/*         */ url: "/images/line_palmtree.png" },
-        { selector: ".watercolor-pink",/*  */ url: "/images/line_watercolor-pink.png" },
-        { selector: ".nails",/*            */ url: "/images/line_nails.png" },
-        { selector: ".galaxy",/*           */ url: "/images/line_galaxy.png" },
-        { selector: ".butterfly-blue",/*   */ url: "/images/butterfly_blue.png" },
-        { selector: ".butterfly-purple",/* */ url: "/images/butterfly_purple.png" },
-        { selector: ".face",/*             */ url: "/images/face.png" },
+        { selector: ".watercolor_bg",/*    */ url: "images/watercolor_bg.png" },
+        { selector: ".clockface", /*       */ url: "images/analog-clockface.png" },
+        { selector: ".clock-helperline",/* */ url: "images/clock_helperline.png" },
+        { selector: ".arc1",/*             */ url: "images/arc1.png" },
+        { selector: ".arc2",/*             */ url: "images/arc2.png" },
+        { selector: ".lion",/*             */ url: "images/line_lion.png" },
+        { selector: ".palmtree",/*         */ url: "images/line_palmtree.png" },
+        { selector: ".watercolor-pink",/*  */ url: "images/line_watercolor-pink.png" },
+        { selector: ".nails",/*            */ url: "images/line_nails.png" },
+        { selector: ".galaxy",/*           */ url: "images/line_galaxy.png" },
+        { selector: ".butterfly-blue",/*   */ url: "images/butterfly_blue.png" },
+        { selector: ".butterfly-purple",/* */ url: "images/butterfly_purple.png" },
+        { selector: ".face",/*             */ url: "images/face.png" },
+        { selector: "#photo10",/*          */ url: "images/photo0.png" },
+        { selector: "#photo9",/*           */ url: "images/photo9.png" },
+        { selector: "#photo8",/*           */ url: "images/photo8.png" },
+        { selector: "#photo7",/*           */ url: "images/photo7.png" },
+        { selector: "#photo6",/*           */ url: "images/photo6.png" },
+        { selector: "#photo5",/*           */ url: "images/photo5.png" },
+        { selector: "#photo4",/*           */ url: "images/photo4.png" },
+        { selector: "#photo3",/*           */ url: "images/photo3.png" },
+        { selector: "#photo2",/*           */ url: "images/photo2.png" },
+        { selector: "#photo1",/*           */ url: "images/photo1.png" },
     ];
     let imagesLoaded = 0;
 
     imageObjs.forEach((imgObj, i, a) => loadImage(imgObj.selector, imgObj.url, () => introProgressBar(a.length, ++imagesLoaded)));
+}
+
+let currPhoto = 0;
+
+function togglePhotos() {
+    currPhoto++;
+    if (currPhoto <= 10) {
+        const photoHolder = document.getElementById("photo-holder");
+        photoHolder.style.visibility = "visible";
+        photoHolder.style.backgroundImage = `url(images/photo${currPhoto}.png)`;
+    }
+    else {
+        document.getElementById("photo-holder").style.visibility = "hidden";
+        currPhoto = 0;
+    }
+    console.log(currPhoto);
 }
 
 function start() {
@@ -70,6 +104,8 @@ function start() {
     setTime();
 
     setInterval(() => setTime(), 1000); // start clock
+
+    document.getElementsByClassName("face")[0].addEventListener("click", () => togglePhotos);
 }
 
 start();
